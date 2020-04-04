c7c25ef250db74b93140e19b1ecec56ef47acfc2
Mantarus <m.mantarus@gmail.com>
2020-04-04 16:13:53 +0300

    Initial commit

---
+++ Assets/Scripts/RoadMover.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class RoadMover : MonoBehaviour
+{
+    public float speed;
+    public float respawnDistance;
+    
+    void Update()
+    {
+        transform.position += Vector3.back * (speed * Time.deltaTime);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("RoadTrigger"))
+        {
+            transform.position += Vector3.forward * respawnDistance;
+        }
+    }
+}
