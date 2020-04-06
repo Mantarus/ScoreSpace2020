5ad8b4a38fe25afea781d70f5ce621a63c1e3177
Mantarus <m.mantarus@gmail.com>
2020-04-06 03:22:23 +0300

    Add pause and restart

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,18 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    private bool _paused = false;
+    private float _lastTimeScale = 1;
+    
     // Update is called once per frame
     void Update()
     {
-        
+        CheckPause();
+        CheckRestart();
+    }
+
+    private void CheckPause()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            _paused = !_paused;
+
+            if (_paused) Pause();
+            else Unpause();
+        }
+    }
+
+    private void Pause()
+    {
+        _lastTimeScale = Time.timeScale;
+        Time.timeScale = 0;
+    }
+
+    private void Unpause()
+    {
+        Time.timeScale = _lastTimeScale;
+    }
+
+    private void CheckRestart()
+    {
+        if (_paused && Input.GetKeyDown(KeyCode.R))
+        {
+            Unpause();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
